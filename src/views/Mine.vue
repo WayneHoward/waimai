1973828cb8537e5a077fb50db064c614e8e9b328
xiaole <xiaole_1019@qq.com>
2021-01-19 14:37:14 +0800

    '14:37'

--- src/views/Mine.vue
+++ src/views/Mine.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-01-16 11:33:57
+ * @LastEditTime: 2021-01-19 13:59:26
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \项目\waimai\waimai\src\views\Mine.vue
+-->
 <template>
     <div class="mine">
         <Header title="我的"></Header>
